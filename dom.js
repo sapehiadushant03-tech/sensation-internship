c536ffa9f80231039fc101216d1532df2b5f982e
Dushant Sapehia <sapehiadushant03@gmail.com>
2026-06-16 11:00:56 +0530

    counter

--- dom.js
+++ dom.js
@@ -1,13 +1,23 @@
-document.getElementById("heading")
-document.getElementsByClassName("heading")
-document.getElementsByTagName("h1")
+// document.getElementById("heading")
+// document.getElementsByClassName("heading")
+// document.getElementsByTagName("h1")
 
 
-document.querySelector("h1")
-document.querySelector("#heading")
-document.querySelector(".heading")
+// document.querySelector("h1")
+// document.querySelector("#heading")
+// document.querySelector(".heading")
 
-document.querySelectorAll("h1")
+// document.querySelectorAll("h1")
 
-
-
+function changeText(){
+let heading = document.querySelector("#heading");
+heading.innerText = "Welcome ";
+heading.style.color ="White";
+document.body.bgColor = "Black";
+}
+function changebtn() {
+    let change = document.querySelector("#Intro");
+    change.innerText = "The change is successfully implemented on the desktop";
+    change.style.color = "Violet";
+    document.body.bgColor = "White";
+}
